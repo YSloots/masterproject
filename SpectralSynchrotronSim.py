5eee94d19db88cc49f52bc61f835eef1de9a39b2
YSloots <yourisloots@gmail.com>
2022-03-28 13:52:14 +0200

    working FB-los integrator, but does need further inspecting

--- SpectralSynchrotronSim.py
+++ SpectralSynchrotronSim.py
@@ -7,11 +7,13 @@ from scipy.special import gamma as gammafunc
 import nifty7 as ift
 
 import astropy.units as u
+from astropy.coordinates import spherical_to_cartesian
 from astropy import constants as cons
 MHz   = 1e6 / u.s
 GHz   = 1e9 / u.s
 me    = cons.m_e.cgs
 c     = cons.c.cgs
+kb    = cons.k_B.cgs
 electron = (cons.e).gauss
 
 # Units definitions since astropy doesnt handle [B]_cgs well
@@ -76,15 +78,6 @@ def plot_slices(data, grid, fname=' '):
     return
 
 
-def make_unitvector_grid(imagine_grid, position):
-    unit = position.unit # need to check that units are the same
-    unitvectors = []
-    for x,y,z in zip(imagine_grid.x.ravel()/unit,imagine_grid.y.ravel()/unit,imagine_grid.z.ravel()/unit):
-        v = np.array([x,y,z])-position/unit
-        unitvectors.append(v/np.linalg.norm(v))
-    return np.reshape(unitvectors, tuple(imagine_grid.resolution)+(3,))*unit
-
-
 #%% Define the Simulator class
 
 class SpectralSynchrotronEmissivitySimulator(Simulator):
@@ -92,105 +85,118 @@ class SpectralSynchrotronEmissivitySimulator(Simulator):
     Simulator for synchrotron emissivity. Assumes a constant powerlaw spectrum
     throughout the entire Galaxy.
     
-    To run the __call__ method one requires to input:
+    Requires the user to define the setup in the los_dictionary    
+    
+    To run the default __call__ method one requires to input:
     - A 3D magnetic_field instance
     - A cosmic_ray_electron_density instance: 3D scalar density grid and constant spectral_index attribute
     
     """
     
     # Class attributes
-    SIMULATED_QUANTITIES = ['sync']
+    SIMULATED_QUANTITIES = ['average_los_brightness']
     REQUIRED_FIELD_TYPES = ['magnetic_field', 'cosmic_ray_electron_density']
     ALLOWED_GRID_TYPES   = ['cartesian']
     
-    def __init__(self, measurements, observing_frequency, observer_position, grid):
+    def __init__(self, measurements, sim_config={'grid':None,'observer':None,'dist':None,'e_dist':None,'lat':None,'lon':None,'FB':None}):
+        
         print("Initializing SynchtrotronEmissivitySimulator")
         # Send the Measurements to the parent class
         super().__init__(measurements) 
         
-        # Stores class-specific attributes (and for now double definitions)
-        # Measurements already contains the observing frequency for the sync data
-        self.observing_frequency = observing_frequency
-        self.grid                = grid # only needed temporarily or for plotting and testing
+        # Write assert and test functions to make sure stuf is correct
+        # - grid.unit == observer.unit
+        # - grid = instance of cartesian grid
+        # - all HII regions should be located within the simulation box        
         
-        # After setting observer position make unit vectorgrid.
-        # Only want to do this once since this will never change 
-        # even when sampling over different fields.
-        unit = observer_position.unit # need to check that grid.unit and position.unit are the same
+        # Stores class-specific attributes (and for now double definitions)
+        for key in mea.keys(): self.observing_frequency = key[1] * GHz
+        grid = sim_config['grid'] # unpack for readability
+        
+        # Setup unitvector grid used for mapping perpendicular component
+        unit = sim_config['observer'].unit 
         unitvectors = []
         for x,y,z in zip(grid.x.ravel()/unit,grid.y.ravel()/unit,grid.z.ravel()/unit):
-            v = np.array([x,y,z])-observer_position/unit
-            #print(v)
+            v = np.array([x,y,z])-sim_config['observer']/unit
             normv = np.linalg.norm(v)
             if normv == 0: # special case where the observer is inside one of the grid points
                 unitvectors.append(v)
             else:
                 unitvectors.append(v/normv)
-        
-        #print(tuple(self.resolution)+(3,))
+        # Save unitvector grid as class attribute
         self.unitvector_grid = np.reshape(unitvectors, tuple(grid.resolution)+(3,))
-        #print(self.unitvector_grid)
     
-
         # Cast imagine instance of grid into a RGSpace grid  
-        unit = grid.x.unit # unit of grid should be the same as unit of observer_position
         cbox = grid.box
         xmax = (cbox[0][1]-cbox[0][0])/unit
         ymax = (cbox[1][1]-cbox[1][0])/unit
-        zmax = (cbox[2][1]-cbox[2][0])/unit
-        translation = np.array([xmax,ymax,zmax])/2     
-        newobserver = observer_position/unit + translation        
-        box         = (xmax,ymax,zmax)
-        #print("Box from grid: ", box)
-        distances   = tuple([b/r for b,r in zip(box, grid.resolution)])
-        #print("Distances (dx,dy,dz): ", distances)
-        self.domain = ift.makeDomain(ift.RGSpace(grid.resolution, distances)) # need this later for the los integration 
+        zmax = (cbox[2][1]-cbox[2][0])/unit       
+        box  = np.array([xmax,ymax,zmax])
+        grid_distances = tuple([b/r for b,r in zip(box, grid.resolution)])
+        self.domain = ift.makeDomain(ift.RGSpace(grid.resolution, grid_distances)) # need this later for the los integration 
     
-        # Get lines of sight from data (for now just test los)
-        nlos   = 10
+        # Get lines of sight from data
+        lat        = sim_config['lat'].to(u.rad)
+        lon        = sim_config['lon'].to(u.rad)
+        hIIdist    = sim_config['dist'].to(u.kpc)
+        e_hIIdist  = sim_config['e_dist'].to(u.kpc)
+        behind     = np.where(np.array(sim_config['FB'])=='B')
+        nlos       = len(hIIdist)
         
+        # Remember the translation
+        translation = np.array([xmax,ymax,zmax])/2 * unit
+        translated_observer = sim_config['observer'] + translation
+
+        # Cast start and end points in a Nifty compatible format
         starts = []
-        [starts.append(newobserver) for dummy in range(nlos)]
-        starts = np.array(starts)
-        ends   = []
-        [ends.append(translation) for dummy in range(nlos)] # towards Galacic center
-        ends   = np.array(ends)
+        for o in translated_observer: starts.append(np.full(shape=(nlos,), fill_value=o)*u.kpc)
+        start_points = np.vstack(starts).T
+
+        ends = []
+        los  = spherical_to_cartesian(r=hIIdist, lat=lat, lon=lon)
+        for i,axis in enumerate(los): ends.append(axis+translated_observer[i])
+        end_points = np.vstack(ends).T
+
+        deltas = end_points - start_points
+        clims  = box * np.sign(deltas) * unit
+        clims[clims<0]=0 # if los goes in negative direction clim of xyz=0-plane
         
-        print(starts[0,:], ends[0,:])
-        # Define the cube integration domain to be called in simulate
-        self.response = self._get_response(starts, ends)
-    
-    # convenience function
+        all_lambdas = (clims-end_points)/deltas # possibly divide by zero
+        lambdas     = np.min(np.abs(all_lambdas), axis=1) # will drop any inf here
+
+        start_points[behind] = end_points[behind] + np.reshape(lambdas[behind], newshape=(np.size(behind),1))*deltas[behind]
+        
+        print("Starts: \n", start_points)   
+        print("Ends:   \n", end_points)
+        
+        self.los_distances = np.linalg.norm(end_points-start_points, axis=1)
+        print("LOS lengths: ", self.los_distances)        
+        
+        nstarts = self._make_nifty_points(start_points)
+        nends   = self._make_nifty_points(end_points)
+
+        self.response = ift.LOSResponse(self.domain, nstarts, nends) # domain doesnt know about its units but starts/ends do?
+
     def _make_nifty_points(self, points, dim=3):
         rows,cols = np.shape(points)
         if cols != dim: # we want each row to be a coordinate (x,y,z)
             points = points.T
             rows   = cols
         npoints = []
-        npoints.append(np.full(shape=(rows,), fill_value=points[:,0]))
-        npoints.append(np.full(shape=(rows,), fill_value=points[:,1]))
-        npoints.append(np.full(shape=(rows,), fill_value=points[:,2]))
+        for d in range(dim):
+            npoints.append(np.full(shape=(rows,), fill_value=points[:,d]))
         return npoints
-
-    def _get_response(self, starts, ends):
-        nstarts = self._make_nifty_points(starts)
-        nends   = self._make_nifty_points(ends)
-        return ift.LOSResponse(self.domain, nstarts, nends)
-        
+    
     def _spectral_integralF(self, mu):
         return 2**(mu+1)/(mu+2) * gammafunc(mu/2 + 7./3) * gammafunc(mu/2+2./3)    
     
     def _spectral_total_emissivity(self, Bper, ncre):
-        vobs  = self.observing_frequency
-        alpha = self.field_parameter_values['cosmic_ray_electron_density']['spectral_index']
-
-        fraction1 = (np.sqrt(3)*electron**3*ncre/(8*np.pi*me*c**2))#.decompose(bases=u.cgs.bases)
-        fraction2 = (4*np.pi*vobs*me*c/(3*electron))#.decompose(bases=u.cgs.bases)
+        vobs      = self.observing_frequency
+        alpha     = self.field_parameter_values['cosmic_ray_electron_density']['spectral_index']
+        fraction1 = (np.sqrt(3)*electron**3*ncre/(8*np.pi*me*c**2))
+        fraction2 = (4*np.pi*vobs*me*c/(3*electron))
         integral  = self._spectral_integralF( (-alpha-3)/2 )
-
-        emissivities = fraction1 * fraction2**((1+alpha)/2) * Bper**((1-alpha)/2) * integral
-        #print(fraction1.unit, ', ', fraction2.unit, ', ', Bper.unit, ', ', emissivities.unit)
-        return emissivities
+        return fraction1 * fraction2**((1+alpha)/2) * Bper**((1-alpha)/2) * integral
     
     def _project_to_perpendicular(self, vectorfield):
         v_parallel      = np.zeros(np.shape(vectorfield)) * vectorfield.unit
@@ -204,7 +210,7 @@ class SpectralSynchrotronEmissivitySimulator(Simulator):
         
     def simulate(self, key, coords_dict, realization_id, output_units):
         """Calculate a 3D box with synchrotron emissivities"""        
-        print("Calling synchrotron simulator")
+        print("Calling synchrotron simulator\n")
         
         # Acces required fields
         alpha     = self.field_parameter_values['cosmic_ray_electron_density']['spectral_index']
@@ -219,65 +225,69 @@ class SpectralSynchrotronEmissivitySimulator(Simulator):
         
         # Calculate grid of emissivity values
         emissivity_grid = self._spectral_total_emissivity(Bperp_amplitude_grid, ncre_grid)
-        print(emissivity_grid)        
+        #print(emissivity_grid, '\n')        
         
         plot_slices(emissivity_grid, self.grid, 'emissivity_cube.png')
         
         # Do the los integration on the domain defined in init with the new emissivity grid
         HII_LOSemissivities = self.response(ift.Field(self.domain, emissivity_grid)).val_rw()
+        HII_LOSemissivities *= emissivity_grid.unit * u.kpc # restore units
+        print("Emissivities: ")
+        print(HII_LOSemissivities, '\n') 
         
-        print(HII_LOSemissivities)
+        # Need to convert to K/kpc units. Note that self.los_distances may not be correct when using e_dist for domain
+        HII_LOSbrightness = c**2/(2*kb*self.observing_frequency**2)*HII_LOSemissivities/self.los_distances
+        print("Average brightness temperature: ")
+        print(HII_LOSbrightness.to(u.K/u.kpc), '\n')
         
-
-        # Initiate Galactic grid that will contain our emissivities
-        #emissivity_grid = np.zeros(self.resolution)
-        # If ncre follows a single powerlaw use spectral emissivity funciton
-        #if ncre_grid.NAME == 'powerlaw_cosmicray_electrons':
-        #    emissivity_grid = 0
-        #else:
-        #    raise TypeError("Incorrect cosmic-ray density field: Expected field of type \'powerlaw_cosmicray_electrons\', received "+ncre_grid.NAME)
-        
-        return np.arange(Npoints) * u.erg.cgs # whatever is returned has to have the same shape as the object in measurements
+        return np.arange(Npoints)*u.K/u.kpc # whatever is returned has to have the same shape as the object in measurements
 
 
 #%% Setup simple test data and physical fields
 
+
+# Setup coordinate grid
+cartesian_grid = img.fields.UniformGrid(box=[[-15*u.kpc, 15*u.kpc],
+                                             [-15*u.kpc, 15*u.kpc],
+                                             [-15*u.kpc, 15*u.kpc]],
+                                             resolution = [61,61,61])
+
 # Fake data dictionary for LOS HII synchrotron measurements
 freq      = 0.09*GHz
-Npoints   = 10
-fake_lat  = 360*np.linspace(0,1,Npoints)*u.deg
-fake_lon  = np.zeros(Npoints)
-fake_j    = np.arange(Npoints)
+Npoints   = 4
+fake_lat  = np.zeros(Npoints)*u.deg
+fake_lon  = np.zeros(Npoints)*u.deg
+
+# custom los for testing:
+fake_lon[2] = 90*u.deg  # positive y-direction
+fake_lon[3] = -90*u.deg # negative y-direction
+fake_lat[1] = 90*u.deg  # positive z-direction
+observer    = np.array([-15,0,0])*u.kpc # from the middle of the 3x3 cube side
+# -> integrating allong the x=-15-plane should give 15-central voxel
+
+fake_j    = np.arange(Npoints) * u.K/u.kpc
 fake_jerr = fake_j/10
 fake_data = {'emissivity': fake_j,
              'err': fake_jerr,
              'lat': fake_lat,
              'lon': fake_lon}
 fake_dset = img.observables.TabularDataset(fake_data,
-                                           name='sync',
-                                           tag='I',
+                                           name='average_los_brightness',
                                            frequency=freq,
-                                           units=u.erg.cgs,
+                                           units=u.K/u.kpc,
                                            data_col='emissivity',
                                            err_col='err',
                                            lat_col='lat',
                                            lon_col='lon')
 mea = img.observables.Measurements(fake_dset)
 
-# Setup coordinate grid
-cartesian_grid = img.fields.UniformGrid(box=[[-15*u.kpc, 15*u.kpc],
-                                             [-15*u.kpc, 15*u.kpc],
-                                             [-15*u.kpc, 15*u.kpc]],
-                                             resolution = [3,3,3])
+
 
 # Cosmic-Ray Electron Model
-powerCRE_exponential = fields.PowerlawCosmicRayElectrons(
-    grid             = cartesian_grid,
-    parameters       ={'scale_radius':5.0*u.kpc,
-                       'scale_height':1.0*u.kpc,
-                       'central_density':314.5*u.cm**-3,
-                       'spectral_index':-3})
-
+constCRE = fields.ConstantCosmicRayElectrons(grid      =cartesian_grid,
+                                             parameters={'density':1.0e37/7.01499*u.cm**-3,
+                                                         'spectral_index':-3})
+credata = constCRE.get_data()
 
 # Magnetic Field Model
 Bfield = fields.ConstantMagneticField(
@@ -291,14 +301,23 @@ Bfield = fields.ConstantMagneticField(
 #%% Call the simulator
 
 vobs     = 90*MHz
-observer = np.array([-20,0,0])*u.kpc
+#observer = np.array([-15,0,0])*u.kpc # observer is defined where lon and lat are too
 
-test_sim   = SpectralSynchrotronEmissivitySimulator(mea,vobs,observer,cartesian_grid) # create instance
-simulation = test_sim([powerCRE_exponential, Bfield]) # call instance again with the required field types
+config = {'grid'    :cartesian_grid,
+          'observer':observer,
+          'dist':np.array([30,15,10,10])*u.kpc,
+          'e_dist':np.ones(Npoints)*0.3*u.kpc,
+          'lat':fake_lat,
+          'lon':fake_lon,
+          'FB':['F','F','B','B']}
+          
+#print(mea)
+test_sim   = SpectralSynchrotronEmissivitySimulator(mea, config) # create instance
+simulation = test_sim([constCRE, Bfield]) # call instance again with the required field types
 
 
 
-print(simulation)
+#print(simulation)
 
 
 
--- losdata.py
+++ losdata.py
@@ -33,7 +33,7 @@ from astropy.io import fits
 datapath = 'data/'
 fname    = 'HII_LOS.fit'
 with fits.open(datapath+fname) as hdul:
-    data = hdul[1].data
+    hiidata = hdul[1].data
 
 #print(type(data), dir(data))
 #print(np.shape(data))
@@ -50,39 +50,48 @@ with fits.open(datapath+fname) as hdul:
 
 head = ['GLON','GLAT','Dist','e_Dist','n_T','eps','e_eps']
 
-#print(data['n_T'][:20])
+print(hiidata['n_T'][:20])
 
 
 # Data dictionary for distances table
-dist_data = {'Dist': np.ones(len(data['Dist'])),
-             'e_Dist': data['e_Dist'],
-             'lat': data['GLAT'],
-             'lon': data['GLON'],
-             'fb' : data['n_T']} # what do do with this column? does this work?
-dist_dset = img.observables.TabularDataset(dist_data,
+dist_data = {'Dist': np.ones(len(hiidata['Dist'])),
+             'e_Dist': hiidata['e_Dist'],
+             'lat': hiidata['GLAT'],
+             'lon': hiidata['GLON'],
+             'fb' : hiidata['n_T']} # what do do with this column? does this work?
+dist_dset = img.observables.TabularDataset(data=dist_data,
                                        name='distances',
                                        units=u.kpc,
                                        data_col='Dist',
                                        err_col='e_Dist',
                                        lat_col='lat',
                                        lon_col='lon')
-mea = img.observables.Measurements(dist_dset)
+#print(dist_dset.keys())
+
 
 # Data dictionary for emissivty table
-em_data = {'emissivity': data['eps'],
-           'err': data['e_eps'],
-           'lat': data['GLAT'],
-           'lon': data['GLON']}
-em_dset = img.observables.TabularDataset(em_data,
-                                       name='sync',
+em_data = {'emissivity': hiidata['eps'],
+           'err': hiidata['e_eps'],
+           'lat': hiidata['GLAT'],
+           'lon': hiidata['GLON']}
+em_dset = img.observables.TabularDataset(data=em_data,
+                                       name='average emissivty',
                                        tag='I',
                                        frequency=0.09*GHz,
-                                       units=u.erg.cgs,
+                                       units=u.K/u.kpc,
                                        data_col='emissivity',
                                        err_col='err',
                                        lat_col='lat',
                                        lon_col='lon')
-mea.append(dataset=em_dset)
+
+#mea = img.observables.Measurements(dist_dset)
+mea = img.observables.Measurements(em_dset)
+#mea.append(dataset=em_dset)
+
+for key in mea.keys():
+    print(mea[key].unit)
+    print(key[1])
+
 
 #%% Accessing the stored data
 
@@ -91,14 +100,18 @@ def investigate_acces():
     for key in mea.keys():
         print(key, '\n', type(mea[key]), '\n', dir(mea[key]), '\n')
     
-    for k in mea.cov.keys():
-        print(k,'\n')
+    #for k in mea.cov.keys():
+    #    print(k,'\n')
     
     distkey = ('distances', None, 'tab', None)
-    print(mea[distkey].data)
-    print(mea[distkey].coords)
-    print(mea.cov[distkey].data.shape)
-
+    #print(mea[distkey].data)
+    #print(mea[distkey].coords)
+    #print(mea.cov[distkey].data.shape)
+    
+    emkey = ('average emissivty', 0.09000000000000001, 'tab', 'I')
+    print(type(mea[emkey].data), '\n', mea[emkey].data)     
+    print(dir(mea[emkey])    )
+    
 #investigate_acces()
 
 #%% Line of sight function
@@ -127,8 +140,8 @@ starts = []
 [starts.append(np.array([-1,-2,-3])) for dummy in range(nlos)]
 starts = np.array(starts)
 
-print(starts)
-print(_get_response(starts))
+#print(starts)
+#print(_get_response(starts))
 
 def get_startpoints(observer, nlos):
     starts = []
@@ -137,7 +150,7 @@ def get_startpoints(observer, nlos):
     return starts
 
 starts = get_startpoints(observer, nlos = 5)
-print(starts)
+#print(starts)
 
 def get_endpoints(gal_lat, gal_lon, distances, dist_error = None):
     """
@@ -170,9 +183,39 @@ ends, e_ends = get_endpoints(gal_lat=lat, gal_lon=lon, distances=distances, dist
 #print(ends)
 #print(e_ends)
 
+#%%
 
 
+def get_endpoints(gal_lat, gal_lon, distances, dist_error = None):
+    """
+    Returns numpy ndarray where each row is a cartesian 
+    galactocentric coordinate in kpc.    
+    """
+    
+    #start = self.observerposition    
+    observer = np.array([-8.5, 0, 0]) * u.kpc
+    los      = spherical_to_cartesian(r=distances, lat=gal_lat, lon=gal_lon)
+    
+    #shift to observer
+    ends = []
+    for i,axis in enumerate(los): ends.append(axis-observer[i])   
+    
+    if type(dist_error) != None:
+        elos = list(spherical_to_cartesian(r=dist_error, lat=gal_lat, lon=gal_lon))
+        return ends, elos #will subtract observer from each row in los
+    else:
+        return ends
 
+
+distkey = ('distances', None, 'tab', None)
+lat = mea[distkey].coords['lat'][:5]/u.deg * np.pi/180 * u.rad
+lon = mea[distkey].coords['lon'][:5]/u.deg * np.pi/180 * u.rad
+distances = mea[distkey].data[0,:5] * u.kpc
+e_dist    = mea.cov[distkey].data[:5,:5].diagonal() * u.kpc
+
+ends, e_ends = get_endpoints(gal_lat=lat, gal_lon=lon, distances=distances, dist_error=e_dist)
+#print(ends)
+#print(e_ends)
 
 
 
